9775225c41348ccd82ba27b552672da6266bb6df
Rob Dobson <rob@dobson.com>
2016-05-15 13:26:16 +0100

    Fixed errors in exception handling around config.json

--- WaitroseService/AccountCoding.py
+++ WaitroseService/AccountCoding.py
@@ -17,16 +17,26 @@ class AccountCoding():
         if len(splitAc) <= 1:
             return None
         acname = splitAc[1]
-        with open('config.json') as configData:
-            config = json.load(configData)
-            if not acname in config:
+        try:
+            configFile = open('config.json')
+
+            try:
+                config = json.load(configFile)
+                configFile.close()
+                if not acname in config:
+                    return None
+                if not "username" in config[acname]:
+                    return None
+                if not "password" in config[acname]:
+                    return None
+                return config[acname]
+            except:
+                logging.error("Config.json cannot be parsed")
+                configFile.close()
                 return None
-            if not "username" in config[acname]:
-                return None
-            if not "password" in config[acname]:
-                return None
-            return config[acname]
+
         except EnvironmentError:
             logging.error("Unable to open Config.json file")
             return None
+            
         return None
